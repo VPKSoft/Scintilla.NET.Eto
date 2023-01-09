1e2324e56a077c0b0ba5ca53e16c56a7b1b34c18
VPKSoft <vpksoft@vpksoft.net>
2023-01-09 06:31:33 +0200

    Squashed 'Scintilla.NET/' content from commit 6b6a8e1
    git-subtree-dir: Scintilla.NET
    git-subtree-split: 6b6a8e14ff6eee4c3da787807dbb2f22523caf93

---
+++ Shared/TabDrawMode.cs
@@ -0,0 +1,17 @@
+﻿namespace ScintillaNET;
+
+/// <summary>
+/// Specifies how tab characters are drawn when whitespace is visible.
+/// </summary>
+public enum TabDrawMode
+{
+    /// <summary>
+    /// The default mode of an arrow stretching until the tabstop.
+    /// </summary>
+    LongArrow = NativeMethods.SCTD_LONGARROW,
+
+    /// <summary>
+    /// A horizontal line stretching until the tabstop.
+    /// </summary>
+    Strikeout = NativeMethods.SCTD_STRIKEOUT
+}
